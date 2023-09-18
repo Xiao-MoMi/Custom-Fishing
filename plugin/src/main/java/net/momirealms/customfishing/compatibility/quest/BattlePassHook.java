0b958ffaec5613598e8cd24d0c5da6e7cd633a88
g2213swo <2057581537@qq.com>
2023-09-18 19:34:33 +0800

    Change BattlePass quest name

--- plugin/src/main/java/net/momirealms/customfishing/compatibility/quest/BattlePassHook.java
+++ plugin/src/main/java/net/momirealms/customfishing/compatibility/quest/BattlePassHook.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) <2022> <XiaoMoMi>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.momirealms.customfishing.compatibility.quest;
 
 import io.github.battlepass.BattlePlugin;
@@ -42,7 +59,7 @@ public class BattlePassHook implements Listener {
 
             // Single Fish Quest
             if (event.getLoot().getID() != null)
-                this.executionBuilder("fish")
+                this.executionBuilder("loot")
                         .player(player)
                         .root(event.getLoot().getID())
                         .progress(event.getAmount())
