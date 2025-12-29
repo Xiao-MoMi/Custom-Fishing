7c4ca048024a35ec365f767fe13b68c0de05161f
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-12-29 02:34:51 +0100

    update betonquest repository url

--- compatibility/build.gradle.kts
+++ compatibility/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
     maven("https://r.irepo.space/maven/") // neigeitems
     maven("https://repo.oraxen.com/releases/") // oraxen
     maven("https://repo.auxilor.io/repository/maven-public/") // eco
-    maven("https://nexus.betonquest.org/repository/betonquest/") // betonquest
+    maven("https://repo.betonquest.org/betonquest/") // betonquest
     maven("https://repo.dmulloy2.net/repository/public/") // betonquest needs packet wrapper?
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://repo.opencollab.dev/main/") // geyser
